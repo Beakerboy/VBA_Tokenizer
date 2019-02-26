1e07bbaec77c7a5c26fd44926188709f01a93e6a
Beakerboy <beakerboy99@yahoo.com>
2019-02-26 17:40:13 +0000

    added more logic

--- src/Tokenizers/VBA.php
+++ src/Tokenizers/VBA.php
@@ -10,57 +10,10 @@
 namespace PHP_CodeSniffer\Tokenizers;
 
 use PHP_CodeSniffer\Util;
+use PHP_CodeSniffer\Tokenizers\PHP;
 
-class VBA extends Tokenizer
+class VBA extends PHP
 {
-
-    /**
-     * A list of tokens that are allowed to open a scope.
-     *
-     * This array also contains information about what kind of token the scope
-     * opener uses to open and close the scope, if the token strictly requires
-     * an opener, if the token can share a scope closer, and who it can be shared
-     * with. An example of a token that shares a scope closer is a CASE scope.
-     *
-     * @var array
-     */
-    public $scopeOpeners = [
-        T_FUNCTION      => [
-            'start'  => "",
-            'end'    => "",
-            'strict' => true,
-            'shared' => false,
-            'with'   => [],
-        ],
-    ];
-
-    /**
-     * A list of tokens that end the scope.
-     *
-     * This array is just a unique collection of the end tokens
-     * from the scopeOpeners array. The data is duplicated here to
-     * save time during parsing of the file.
-     *
-     * @var array
-     */
-    public $endScopeTokens = [
-    ];
-
-    /**
-     * Known lengths of tokens.
-     *
-     * @var array<int, int>
-     */
-    public $knownLengths = [
-    ];
-    
-    /**
-     * A cache of different token types, resolved into arrays.
-     *
-     * @var array
-     * @see standardiseToken()
-     */
-    private static $resolveTokenCache = [];
 
     protected function convertFile($string)
     {
@@ -69,22 +22,71 @@ class VBA extends Tokenizer
         foreach ($string_array as $line) {
             $line_tokens = token_get_all("<?php\r\n" . $line);
             array_shift($line_tokens);
-            foreach ($line_tokens as $key=>$token) {
+            foreach ($line_tokens as $key=>&$token) {
                 if ($token[0] === T_ENCAPSED_AND_WHITESPACE) {
                     $token[1] = '//' . substr($token[1], 1);      
+                }
+                // Turn Subs into Functions
+                else if ($token[0] === T_STRING && ($token[1] == "Sub" || $token[1] == "Property")) {
+                    $token[1] = "Function";
+                }
+                else if ($token[0] == T_BITWISE_AND) {
+                    $token[1] = '.';
+                }
+                else if ($token[0] === T_STRING && $token[1] == "NOT") {
+                    $token[1] = '!';
+                }
+                else if ($token[0] === T_STRING && $token[1] == "AND") {
+                    $token[1] = '&&';
+                }
+                else if ($token[0] === T_STRING && $token[1] == "OR") {
+                    $token[1] = '||';
+                }
+                else if ($token[0] === T_STRING && $token[1] == "Then") {
+                    $token[1] = 'static';
+                }
+                else if ($token == ".") {
+                    $token = [T_STRING, '->'];
                 }
                 // If a string with the value "End" is found, change it to a special enddeclare if it is
                 // followed by Property, Function, or Sub.
                 // If it is follow by "if", change it to "endif"
-                if ($token[0] === T_STRING && $token[1] === "End") {
+                else if ($token[0] === T_STRING && $token[1] === "End") {
                     $next_tag =  $line_tokens[$key + 2][1];
                     if ($next_tag == "Function" || $next_tag == "Sub" || $next_tag == "Property") { 
                         $token[1] = "enddeclare";
+                        next($line_tokens);
+                        $next_key = key($line_tokens);
+                        next($line_tokens);
+                        $second_key = key($line_tokens);
+                        unset($line_tokens[$next_key]);
+                        unset($line_tokens[$second_key]);
                     } elseif ($next_tag == 'If') {
                         $token[1] = "endif";
-                        unset($line_tokens[$key+1]);
-                        unset($line_tokens[$key+2]);
+                        next($line_tokens);
+                        $next_key = key($line_tokens);
+                        next($line_tokens);
+                        $second_key = key($line_tokens);
+                        unset($line_tokens[$next_key]);
+                        unset($line_tokens[$second_key]);
                     }
+                }
+                else if ($token[0] == T_FOR) {
+                    $next_tag =  $line_tokens[$key + 2][1];
+                    if ($next_tag == "Each") { 
+                        $token[1] = 'foreach';
+                        next($line_tokens);
+                        $next_key = key($line_tokens);
+                        next($line_tokens);
+                        $second_key = key($line_tokens);
+                        unset($line_tokens[$next_key]);
+                        unset($line_tokens[$second_key]);
+                        next($line_tokens);
+                    }
+                }
+                // A for loop ends with Next i while a foreach ends with Next
+                else if ($token[0] == T_STRING && $token[1] == "Next") {
+                        $token[1] = '}';
                 }
                 // Write the token value back to the new string
                 if (isset($token[1])) {
@@ -110,185 +112,66 @@ class VBA extends Tokenizer
      */
     protected function tokenize($string)
     {
-        if (PHP_CODESNIFFER_VERBOSITY > 1) {
-            echo "\t*** START VBA TOKENIZING ***".PHP_EOL;
-            $isWin = false;
-            if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-                $isWin = true;
-            }
-        }
+        $this->scopeOpeners[T_CLASS] =
+        [
+            'start'  => [T_STRING=> T_STRING],
+            'end'    => [T_CLOSE_CURLY_BRACKET => T_CLOSE_CURLY_BRACKET],
+            'strict' => true,
+            'shared' => false,
+            'with'   => [],
+        ];
+        $this->scopeOpeners[T_IF] = 
+        [
+            'start'  => [T_STATIC, T_STATIC],  // Should be T_THEN
+            'end'    => [
+                T_ENDIF               => T_ENDIF,
+                T_ELSE                => T_ELSE,
+                T_ELSEIF              => T_ELSEIF,
+            ],
+            'strict' => false,
+            'shared' => false,
+            'with'   => [
+                T_ELSE   => T_ELSE,
+                T_ELSEIF => T_ELSEIF,
+            ],
+        ];
+        // T_ELSE
+        // T_ELSEIF
+        $this->scopeOpeners[T_FUNCTION] = 
+        [
+            'start'  => [T_CLOSE_PARENTHESIS => T_CLOSE_PARENTHESIS],  //Should be newline
+            'end'    => [T_ENDDECLARE => T_ENDDECLARE],
+            'strict' => false,
+            'shared' => false,
+            'with'   => [],
+        ];
+        $this->scopeOpeners[T_FOREACH] = 
+        [
+            'start'  => [
+                T_WHITESPACE=> T_WHITESPACE,
+            ],
+            'end'    => [
+                T_CLOSE_CURLY_BRACKET => T_CLOSE_CURLY_BRACKET,
+            ],
+            'strict' => false,
+            'shared' => false,
+            'with'   => [],
+        ];
+        $this->scopeOpeners[T_FOR] = 
+        [
+            'start'  => [
+                T_WHITESPACE=> T_WHITESPACE,
+            ],
+            'end'    => [
+                T_CLOSE_CURLY_BRACKET => T_CLOSE_CURLY_BRACKET,
+            ],
+            'strict' => false,
+            'shared' => false,
+            'with'   => [],
+        ];
         $string = $this->convertFile($string);
-        $tokens      = @token_get_all($string);
-        array_shift($tokens);
-        $finalTokens = [];
-        $newStackPtr       = 0;
-        $numTokens         = count($tokens);
-        $lastNotEmptyToken = 0;
-        $insideInlineIf = [];
-        $insideUseGroup = false;
-        $commentTokenizer = new Comment();
-
-        for ($stackPtr = 0; $stackPtr < $numTokens; $stackPtr++) {
-            // Special case for tokens we have needed to blank out.
-            if ($tokens[$stackPtr] === null) {
-                continue;
-            }
-            $token        = (array) $tokens[$stackPtr];
-            $tokenIsArray = isset($token[1]);
-            if (PHP_CODESNIFFER_VERBOSITY > 1) {
-                if ($tokenIsArray === true) {
-                    $type    = Util\Tokens::tokenName($token[0]);
-                    $content = Util\Common::prepareForOutput($token[1]);
-                } else {
-                    $newToken = self::resolveSimpleToken($token[0]);
-                    $type     = $newToken['type'];
-                    $content  = Util\Common::prepareForOutput($token[0]);
-                }
-                echo "\tProcess token ";
-                if ($tokenIsArray === true) {
-                    echo "[$stackPtr]";
-                } else {
-                    echo " $stackPtr ";
-                }
-                echo ": $type => $content";
-            }//end if
-
-            if (PHP_CODESNIFFER_VERBOSITY > 1) {
-                echo PHP_EOL;
-            }
-        }//end for
-
-        if (PHP_CODESNIFFER_VERBOSITY > 1) {
-            echo "\t*** END PHP TOKENIZING ***".PHP_EOL;
-        }
-
-        return $finalTokens;
+        return parent::tokenize($string);
     }
 
-    /**
-     * Performs additional processing after main tokenizing.
-     *
-     * This additional processing checks for CASE statements that are using curly
-     * braces for scope openers and closers. It also turns some T_FUNCTION tokens
-     * into T_CLOSURE when they are not standard function definitions. It also
-     * detects short array syntax and converts those square brackets into new tokens.
-     * It also corrects some usage of the static and class keywords. It also
-     * assigns tokens to function return types.
-     *
-     * @return void
-     */
-    protected function processAdditional()
-    {
-        if (PHP_CODESNIFFER_VERBOSITY > 1) {
-            echo "\t*** START ADDITIONAL VBA PROCESSING ***".PHP_EOL;
-        }
-    }
-
-    /**
-     * Converts simple tokens into a format that conforms to complex tokens
-     * produced by token_get_all().
-     *
-     * Simple tokens are tokens that are not in array form when produced from
-     * token_get_all().
-     *
-     * @param string $token The simple token to convert.
-     *
-     * @return array The new token in array format.
-     */
-    public static function resolveSimpleToken($token)
-    {
-        $newToken = [];
-
-        switch ($token) {
-        case '{':
-            $newToken['type'] = 'T_OPEN_CURLY_BRACKET';
-            break;
-        case '}':
-            $newToken['type'] = 'T_CLOSE_CURLY_BRACKET';
-            break;
-        case '[':
-            $newToken['type'] = 'T_OPEN_SQUARE_BRACKET';
-            break;
-        case ']':
-            $newToken['type'] = 'T_CLOSE_SQUARE_BRACKET';
-            break;
-        case '(':
-            $newToken['type'] = 'T_OPEN_PARENTHESIS';
-            break;
-        case ')':
-            $newToken['type'] = 'T_CLOSE_PARENTHESIS';
-            break;
-        case ':':
-            $newToken['type'] = 'T_COLON';
-            break;
-        case '.':
-            $newToken['type'] = 'T_STRING_CONCAT';
-            break;
-        case ';':
-            $newToken['type'] = 'T_SEMICOLON';
-            break;
-        case '=':
-            $newToken['type'] = 'T_EQUAL';
-            break;
-        case '*':
-            $newToken['type'] = 'T_MULTIPLY';
-            break;
-        case '/':
-            $newToken['type'] = 'T_DIVIDE';
-            break;
-        case '+':
-            $newToken['type'] = 'T_PLUS';
-            break;
-        case '-':
-            $newToken['type'] = 'T_MINUS';
-            break;
-        case '%':
-            $newToken['type'] = 'T_MODULUS';
-            break;
-        case '^':
-            $newToken['type'] = 'T_BITWISE_XOR';
-            break;
-        case '&':
-            $newToken['type'] = 'T_BITWISE_AND';
-            break;
-        case '|':
-            $newToken['type'] = 'T_BITWISE_OR';
-            break;
-        case '~':
-            $newToken['type'] = 'T_BITWISE_NOT';
-            break;
-        case '<':
-            $newToken['type'] = 'T_LESS_THAN';
-            break;
-        case '>':
-            $newToken['type'] = 'T_GREATER_THAN';
-            break;
-        case '!':
-            $newToken['type'] = 'T_BOOLEAN_NOT';
-            break;
-        case ',':
-            $newToken['type'] = 'T_COMMA';
-            break;
-        case '@':
-            $newToken['type'] = 'T_ASPERAND';
-            break;
-        case '$':
-            $newToken['type'] = 'T_DOLLAR';
-            break;
-        case '`':
-            $newToken['type'] = 'T_BACKTICK';
-            break;
-        default:
-            $newToken['type'] = 'T_NONE';
-            break;
-        }//end switch
-
-        $newToken['code']    = constant($newToken['type']);
-        $newToken['content'] = $token;
-
-        self::$resolveTokenCache[$token] = $newToken;
-        return $newToken;
-
-    }//end resolveSimpleToken()
 }
 
