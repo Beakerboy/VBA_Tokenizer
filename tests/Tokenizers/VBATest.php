57a228090fad45096983650dbf9f98579ebf1e46
Beakerboy <beakerboy99@yahoo.com>
2019-02-28 16:48:13 +0000

    added tests

---
+++ tests/Tokenizers/VBATest.php
@@ -0,0 +1,20 @@
+<?php
+namespace PHP_CodeSniffer\Tests\Tokenizer;
+
+use PHPCodeSniffer\Tokenizers\VBA;
+
+class VBATest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @testCase     Tests that the tokenizer returns the correct array
+     */
+    public function testTokenizer()
+    {
+        global $argv;
+        $argv = ["phpcs", "--extensions=cls/vba", "--standards=../../src/Standards/VBA"];
+        $string = file_get_contents("../Test.cls");
+        $runner   = new PHP_CodeSniffer\Runner();
+        $exitCode = $runner->runPHPCS();
+        $this->assertEquals(0, $exit_code);
+    }
+}
