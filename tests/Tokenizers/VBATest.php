aa64404f6e9c2d666e57221fe713cd38ce7f32f4
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-02-28 06:12:45 -0500

    Create VBATest.php

---
+++ tests/Tokenizers/VBATest.php
@@ -0,0 +1,16 @@
+<?php
+namespace PHP_CodeSniffer\Tests\Tokenizer;
+
+use PHPCodeSniffer\Tokenizers\VBA;
+class VBATest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @testCase     Tests that the tokenizer returns the correct array
+     */
+    public function testTokenizer()
+    {
+        // Read file into a string.
+        // Initialize the VBA class
+        // Ensure it exits without warnings
+    }
+}
