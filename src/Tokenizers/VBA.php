c92e4d31e70cd04465dc269f37ae2615a1c9d5d2
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-02-28 08:03:38 -0500

    Update VBA.php

--- src/Tokenizers/VBA.php
+++ src/Tokenizers/VBA.php
@@ -8,6 +8,7 @@ namespace PHP_CodeSniffer\Tokenizers;
 
 define('T_THEN', 'PHPCS_T_THEN');
 define('T_BEGIN', 'PHPCS_T_BEGIN');
+define('T_ATTRIBUTE', 'PHPCS_T_ATTRIBUTE');
 define('T_EOL', 'PHPCS_T_EOL');
 define('T_NEXT', 'PHPCS_T_NEXT');
 define('T_END_FUNCTION', 'PHPCS_T_END_FUNCTION');
