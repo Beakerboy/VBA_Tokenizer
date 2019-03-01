672ea1654d818088676ffb4c0564937c84a383a8
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-03-01 10:53:24 -0500

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,3 +5,4 @@ if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
     require_once(__DIR__ . '/../../../autoload.php');
 }
 require_once(__DIR__ . '/../vendor/squizlabs/php_codesniffer/autoload.php');
+include(__DIR__ . '/../vendor/squizlabs/php_codesniffer/src/Util/Tokens.php');
