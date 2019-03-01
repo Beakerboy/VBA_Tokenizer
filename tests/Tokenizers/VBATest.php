eb58b314e73b7c3db22aaff6f7404f1f031da808
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-03-01 14:32:59 -0500

    Update VBATest.php

--- tests/Tokenizers/VBATest.php
+++ tests/Tokenizers/VBATest.php
@@ -14,14 +14,50 @@ class VBATest extends \PHPUnit\Framework\TestCase
 {
     /**
      * @testCase     Tests that the tokenizer returns the correct array
+     * @dataProvider dataProviderForTokenizer
      */
-    public function testTokenizer()
+    public function testTokenizer($string, $expected)
     {
         $config = new Config(['--extensions=cls/vba']);
-        $string = file_get_contents('tests/Test.cls');
-        $VBA = new GenericVBAExtension($string, $config, '\r\n');
+        $VBA = new GenericVBAExtension($string, $config, '\n');
         $VBA->callTokenizer();
         $tokens = $VBA->getTokens();
-        $this->assertEquals($tokens, $tokens);
+        $this->assertEquals($expected, $tokens);
+    }
+    
+    public function dataProviderForTokenizer()
+    {
+        $input1 = "' Function: Foo
+Public Function Foo(iVariable As Double) As Boolean
+    While iVariable Is 2
+        iVariable = iVariable + 1
+    Wend
+End Function";
+        
+        $output1 = [
+            [T_COMMENT, '// Function: Foo\n', 1],
+            [T_PUBLIC, 'Public', 2], [T_WHITESPACE, ' ', 2], 
+            [T_FUNCTION, 'Function', 2], [T_WHITESPACE, ' ', 2],
+            [T_STRING, 'Foo', 2],
+            [T_LEFT_PARENTHESIS, '(', 2], [T_STRING, 'iVariable', 2], [T_WHITESPACE, ' ', 2],
+            [T_AS, 'As', 2],[T_WHITESPACE, ' ', 2],
+            [T_STRING, 'Double', 2], [T_RIGHT_PARENTHESIS, ')', 2], [T_WHITESPACE, ' ', 2],
+            [T_AS], [T_WHITESPACE, ' ', 2],
+            [T_STRING, 'Boolean', 2],[T_WHITESPACE, '\n    ', 2],
+            [T_WHILE, 'While', 3], [T_WHITESPACE, ' ', 3],
+            [T_STRING, 'iVariable', 3], [T_WHITESPACE, ' ', 3],
+            [T_IS_IDENTICAL, '===', 3], [T_WHITESPACE, ' ', 3],
+            [T_NUMBER, '2', 3], [T_WHITESPACE, '\n        ', 3],
+            [T_STRING, 'iVariable', 3], [T_WHITESPACE, ' ', 3],
+            [T_EQUALS, '=', 4], [T_WHITESPACE, ' ', 4],
+            [T_STRING, 'iVariable', 4], [T_WHITESPACE, ' ', 4],
+            [T_PLUS, '+', 4], [T_WHITESPACE, ' ', 4],
+            [T_NUMBER, '1', 3], [T_WHITESPACE, '\n    ', 3],
+            [T_STATIC, 'static', 5], [T_WHITESPACE, '\n', 5],
+            [T_ENDDECLARE, 'enddeclare', 6],
+        ];
+        return [
+            [$input1, $output1],
+        ];
     }
 }
