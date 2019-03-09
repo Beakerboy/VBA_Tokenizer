da771793d99cacdd5eeb7270b6fc5e47bf1d870e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-03-09 13:17:20 -0500

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,4 +6,4 @@ if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
 }
 require_once(__DIR__ . '/../vendor/squizlabs/php_codesniffer/autoload.php');
 include(__DIR__ . '/../vendor/squizlabs/php_codesniffer/src/Util/Tokens.php');
-include(__DIR__ . '/src/Tokenizers/VBANew.php');
+include(__DIR__ . '/../src/Tokenizers/VBANew.php');
